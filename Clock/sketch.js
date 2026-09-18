6fd425fc2b22a6ca41e881fe854eca97a1869416
Silas Howe <s_howe@coloradocollege.edu>
2026-09-18 11:35:34 -0600

    function to create predictable gaps in boundaries

--- Clock/sketch.js
+++ Clock/sketch.js
@@ -27,8 +27,27 @@ function setup () {
   // boundaries.push(new Boundary(100, height / 1.2, 30, height, 0, false))
   // boundaries.push(new Boundary(10, height / 2, 30, height, 0, false))
   // cups.push(new Cup(400, 100, 100, 100, 10, true))
-  boundaries.push(new Boundary(0, 100, 300, 20, 0))
-  boundaries.push(new Boundary(410, 100, 200, 20, 0))
+  boundaries.push(new Boundary(0, 60, 300, 20, 0.1)) // right edge at 150
+  let gap = 10
+  for (let i = 0; i < 4; i++) {
+    last_bound = boundaries[boundaries.length - 1]
+    console.log(last_bound)
+    new_coords = calcBoundary(
+      last_bound.x,
+      last_bound.y,
+      last_bound.w,
+      100, // new width
+      last_bound.body.angle,
+      gap // gap
+    )
+    boundaries.push(new Boundary(new_coords.x, new_coords.y, 100, 20, new_coords.a))
+    gap += 10
+  }
+
+  //boundaries.push(new Boundary(390, 175, 100, 20, 0.3))
+  //boundaries.push(new Boundary(510, 212, 100, 20, 0.3))
+
+  console.log(new_coords)
 }
 
 // function mousePressed () {
@@ -36,7 +55,11 @@ function setup () {
 //     circles.push(new Circle(mouseX, mouseY, random(5, 10)))
 //   }
 // }
-
+function calcBoundary (prev_x, prev_y, prev_w, new_w, a, gap) {
+  dX = prev_w / 2 + gap + new_w / 2
+  yNext = prev_y + tan(a) * dX
+  return { x: prev_x + dX, y: yNext, a: a }
+}
 function moveCup (cup) {
   const newHeight = cup.y - (((frameCount / 60) * 6.67) % height)
   if (cup.moves) {
@@ -68,13 +91,22 @@ function draw () {
   //   circles.push(newBall)
   // }
   let now_second = second()
-  if(now_second != current_second){
-    let newBall = new Circle(0, 20, random(5, 10))
-    Body.setVelocity(newBall.body, { x: 2, y: 0 })
+  let big_second = second()
+  if (now_second != current_second) {
+    let newBall = new Circle(0, 50, 10)
+    Body.setVelocity(newBall.body, { x: 3, y: 0 })
     circles.push(newBall)
+    if (now_second % 2) {
+      let newBall2 = new Circle(0, 50, 20)
+      Body.setVelocity(newBall2.body, { x: 3, y: 0 })
+      circles.push(newBall2)
+    }
     current_second = now_second
   }
-  
+
+  console.log('current: ' + current_second)
+  console.log('now: ' + now_second)
+  console.log('big: ' + big_second)
   for (let i = 0; i < circles.length; i++) {
     circles[i].show()
   }
